4e6feeb31da8701867ec46f71a83740819927ec1
Tersoo Abraham <isaacabraham908@yahoo.com>
2022-12-08 16:37:58 -0800

    Task 5 done

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Entry Point
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	printf("with a proper grammar, but the out come is a piece of art");
+	return (0);
+}	
