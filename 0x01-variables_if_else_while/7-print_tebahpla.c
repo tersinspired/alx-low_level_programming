abd9f6a21c07ca197c28dd047cfb197d19f4204b
Tersoo Abraham <isaacabraham908@yahoo.com>
2022-12-09 15:47:42 -0800

    Multiple task accomplished

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - return an alphabet in reverse order
+ *
+ * Return: Always 0 (Success)
+ *
+ */
+
+int main(void)
+
+{
+	int ch = 'z';
+
+	while (ch >= 'a')
+	{
+		putchar(ch);
+		ch--;
+	}
+
+	putchar('\n');
+	return (0);
+
+}
