ac702147322ece2479c4f1d37b1a877f7006c0fe
Tersoo Abraham <isaacabraham908@yahoo.com>
2022-12-08 16:45:53 -0800

    Task 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -5,10 +5,8 @@
  *
  * Return: Always 0 (success)
  */
-
 int main(void)
 {
-	printf("with a proper grammar, but the outcome is a piece of art");
+	printf("with proper grammar, but the outcome is a piece of art");
 	return (0);
-
 }	
