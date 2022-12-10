cdedca0c219332b43493e3faae01090f55f35542
Tersoo Abraham <isaacabraham908@yahoo.com>
2022-12-09 16:29:07 -0800

    Task 12

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Print all possible different combination of two digits
+ *
+ * Return:: Always 0 (Success)
+ *
+ */
+
+int main(void)
+
+{
+	int ones = '0';
+	int tens = '0';
+
+	for  (tens = '0'; tens <= '9'; tens++) /*prints tens digit*/
+	{
+		for (ones = '0'; ones <= '9'; ones++) /*prints ones digit*/
+		{
+			if (!((ones == tens) || (tens > ones))) /*eliminate repititions*/
+			{
+				putchar(tens);
+				putchar(ones);
+				if (!(ones == '9' && tens == '8')) /*adds commas and space*/
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+
+	return (0);
+
+}
