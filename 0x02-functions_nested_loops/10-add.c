278908cbedfcfff3fd421e8fd50d12e3c2f724c1
Tersoo Abraham <isaacabraham908@yahoo.com>
2022-12-13 09:29:05 -0800

    Task 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * add - Entry point for the addiction
+ * @a: one value to be added together
+ * @b: second value to be added
+ * Return: Always 0 (Success)
+ */
+
+in add(int a, int b)
+
+{
+	return (a + b);
+}
