9b426926163efc3cac20be8e888f727332f3d4ba
Tersoo Abraham <isaacabraham908@yahoo.com>
2022-12-13 10:52:38 -0800

    Task 13

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#incude "studio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: computes the sum of all
+ * 		the multiples of 3 or 5
+ * 		below 1024 (excluded)
+ *
+ * 		return: Always 0 (success)
+ */
+
+int main(void)
+
+{
+	int sum, num;
+
+	for (num = 0; num < 1024; ++num)
+	{
+		if ((num % 3 == 0) || (num % 5 == 0))
+			sum += num;
+	}
+	printf("%d\n", sum);
+
+	return (0);
+}
