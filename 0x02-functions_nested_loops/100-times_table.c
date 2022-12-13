e41d0993159f354524d3ce2e74dacedc3614b762
Tersoo Abraham <isaacabraham908@yahoo.com>
2022-12-13 12:08:36 -0800

    Task 12

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -37,14 +37,14 @@ void print_times_table(int n)
 						_putchar(' ');
 						_putchar('0' + (product / 10));
 						_putchar('0' + (product % 10));
-					}
-					else if (product >= 100)
-					{
-						_putchar('0' + (product / 100));
-						_putchar('0' + ((product / 10) % 10));
-						_putchar('0' + (product % 10));
-					}
 				}
+				else if (product >= 100)
+				{
+					_putchar('0' + (product / 100));
+					_putchar('0' + ((product / 10) % 10));
+					_putchar('0' + (product % 10));
+				}
+			
 			}
 			_putchar('\n');
 		}
