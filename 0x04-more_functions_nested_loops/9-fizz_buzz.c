135f85a5c37234bd0c602238509aa54462092241
Tersoo Abraham <isaacabraham908@yahoo.com>
2022-12-15 07:55:39 -0800

    Task 9

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -12,15 +12,23 @@ int main(void)
 	for (i = 1; i < 100; i++)
 	{
 		if (i % 3 == 0 && i % 5 == 0)
+		{
 			printf("Fizzbuzz ");
+		}
 		else if (i % 3 == 0)
+		{
 			printf("Fizz ");
+		}
 		else if (i % 5 == 0)
+		{
 			printf("Buzz ");
+		}
 		else
+		{
 			printf("%d ", i);
+		}
 	}
-	printf("Buzz\n");
+	printf("\n");
 	return (0);
 
 }
