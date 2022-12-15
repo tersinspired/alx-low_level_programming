b637751b2f4b38536f0667642f66801829812370
Tersoo Abraham <isaacabraham908@yahoo.com>
2022-12-15 08:49:12 -0800

    Task 9

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,34 +1,36 @@
 #include <stdio.h>
 
 /**
- * main - fizzbuzz
- * Return: 0
+ * main - prints Buzz each numbers of 3 and 5
+ * Return: Always 0.
  */
 
 int main(void)
 {
-	int i;
+	int n;
 
-	for (i = 1; i < 100; i++)
+	n = 1;
+	printf("%d", n);
+	for (n = 2; n <= 100; n++)
 	{
-		if (i % 3 == 0 && i % 5 == 0)
+		if ((n % 3 == 0) && (n % 5 == 0))
 		{
-			printf("Fizzbuzz ");
+			printf(" FizzBuzz");
 		}
-		else if (i % 3 == 0)
+		else if (n % 3 == 0)
 		{
-			printf("Fizz ");
+			printf(" Fizz");
 		}
-		else if (i % 5 == 0)
+		else if (n % 5 == 0)
 		{
-			printf("Buzz ");
+			printf(" Buzz");
 		}
 		else
 		{
-			printf("%d ", i);
+			printf(" %d", n);
 		}
+
 	}
 	printf("\n");
 	return (0);
-
 }
