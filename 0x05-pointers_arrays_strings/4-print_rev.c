77b2a0ad1f8223d1581b9976d6a58a35bc6e2790
Tersoo Abraham <isaacabraham908@yahoo.com>
2022-12-19 05:08:25 -0800

    Task 4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_rev - this function prints a string in reverse order
+ * @s: pointer parameter
+ */
+
+void print_rev(char *s)
+{
+	ing i = 0;
+	int j;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	for (j = i - 1; j >= 0; j--)
+	{
+		_putchar(s[j]);
+	}
+	_putchar('\n;);
+}
